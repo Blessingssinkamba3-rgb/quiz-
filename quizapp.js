8b182a6b2e5b9014a0255c6e1643eb6cf99ce7a5
Blessingssinkamba3-rgb <blessingssinkamba3@gmail.com>
2025-10-25 02:05:08 +0200

    Add files via upload

---
+++ quizapp.js
@@ -0,0 +1,13 @@
+const questions = [
+    question: "How many provinces are in zambia",
+    answers:[
+        {text : "10",
+         correct:true}
+            {text : "11",
+         correct:false }
+            {text : "15",
+         correct:false }
+            {text : "19",
+         correct:false }
+    ]
+]
